5470e3668851801ef33316785068c6f966712064
stillho <yangdehuadao@gmail.com>
2021-11-30 16:35:17 +0900

    commit10

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,7 @@ class CommentsController < ApplicationController
      redirect_to prototype_path(@comment.prototype)
     else
       @prototype = Prototype.find(params[:prototype_id])
+      @comment = Comment.new
       @comments = @prototype.comments.includes(:user)
      render "prototypes/show"
     end
